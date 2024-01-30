273a3dd8c82ce74b05d76bb4a7050af972fc2e82
crazydeveloper914 <crazydeveloper914@gmail.com>
2024-01-30 17:36:27 +0530

    integrate with rust webserver

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 use dioxus::prelude::*;
 
 fn main() {
+    wasm_logger::init(wasm_logger::Config::default());
     // launch the web app
     dioxus_web::launch(App);
 }
@@ -15,6 +16,22 @@ fn App(cx: Scope) -> Element {
             div {
                 "Hello, world!",
             }
+            button {
+                class: "btn btn-primary",
+                onclick: |_| on_click_button(),
+                r#type: "button",
+                "Click Me"
+            }
         }
     })
+}
+
+async fn on_click_button() {
+    let response = reqwest::get("http://127.0.0.1:3000/get")
+        .await
+        .unwrap()
+        .text()
+        .await;
+
+    log::info!("{:?}", response);
 }
